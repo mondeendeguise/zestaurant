b76c3093fa8e409f0c137201f05c9c8bc2c41011
Ash Bullock <mondeendeguise@gmail.com>
2025-04-20 09:50:30 -0600

    test: parse lastUpdated to timestamp

--- toast/toast_test.go
+++ toast/toast_test.go
@@ -7,6 +7,7 @@ import (
 
 	"embed"
 	"encoding/json"
+	"time"
 
 	"github.com/Rican7/conjson"
 	"github.com/Rican7/conjson/transform"
@@ -15,17 +16,62 @@ import (
 //go:embed mock
 var mockJson embed.FS
 
-const menusFile string = "mock/menus.json"
+const METADATA_FILE string = "mock/metadata.json"
+const MENUS_FILE string = "mock/menus.json"
 
-// TODO: handle Metadata.LastUpdated, Restaurant.LastUpdated as timestamps
-//       check that we only send GET requests when we're out of date
+// https://stackoverflow.com/a/38596248
+const TIME_LAYOUT_ISO8601 string = "2006-01-02T15:04:05-0700"
 
 // TODO: abstract some of this away
 //       create a get() function that supports dependency injection
-func TestUnmarshalMenus(t *testing.T) {
-	b, err := mockJson.ReadFile(menusFile)
+
+func TestGetMetadata(t *testing.T) {
+	b, err := mockJson.ReadFile(METADATA_FILE)
 	if err != nil {
-		t.Errorf("failed to read file `%s`\n", menusFile)
+		t.Errorf("failed to read file `%s`", METADATA_FILE)
+	}
+
+	metadata := menus.Metadata{}
+	var unmarshaler json.Unmarshaler = conjson.NewUnmarshaler(&metadata, transform.ConventionalKeys())
+
+	err = json.Unmarshal(b, unmarshaler)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+}
+
+// TODO: ensure we only send GET requests when `lastUpdated` is modified
+
+func TestCheckLastUpdated(t *testing.T) {
+	b, err := mockJson.ReadFile(METADATA_FILE)
+	if err != nil {
+		t.Errorf("failed to read file `%s`", METADATA_FILE)
+	}
+
+	metadata := menus.Metadata{}
+	var unmarshaler json.Unmarshaler = conjson.NewUnmarshaler(&metadata, transform.ConventionalKeys())
+
+	err = json.Unmarshal(b, unmarshaler)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	lastUpdated, err := time.Parse(TIME_LAYOUT_ISO8601, metadata.LastUpdated)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	var timestamp time.Time = time.Now()
+
+	if !lastUpdated.Before(timestamp) {
+		t.Errorf("lastUpdated has an impossible timestamp")
+	}
+}
+
+func TestUnmarshalMenus(t *testing.T) {
+	b, err := mockJson.ReadFile(MENUS_FILE)
+	if err != nil {
+		t.Errorf("failed to read file `%s`", MENUS_FILE)
 	}
 
 	restaurant := menus.Restaurant{}
@@ -33,6 +79,6 @@ func TestUnmarshalMenus(t *testing.T) {
 
 	err = json.Unmarshal(b, unmarshaler)
 	if err != nil {
-		t.Errorf("%s\n", err)
+		t.Errorf("%s", err)
 	}
 }
