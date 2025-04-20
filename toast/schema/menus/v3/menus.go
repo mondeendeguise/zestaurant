322d2e6911dc50b7e62ade9a4156145fc2aa7461
Ash Bullock <mondeendeguise@gmail.com>
2025-04-20 01:10:57 -0600

    fix: remove broken menus common dependency mess

--- toast/schema/menus/v3/menus.go
+++ toast/schema/menus/v3/menus.go
@@ -1,11 +1,122 @@
 package menus
 
-import (
-	"github.com/mondeendeguise/zestaurant/toast/schema/menus"
-)
-
 type strings []string
 type ints []int
+
+type TimeRanges []TimeRange
+type TimeRange struct {
+	Start string
+	End string
+}
+
+type Schedules []Schedule
+type Schedule struct {
+	Days strings
+	TimeRanges TimeRanges
+}
+
+type Availability struct {
+	AlwaysAvailable bool
+	Schedule Schedules
+}
+
+type ItemTags []ItemTag
+type ItemTag struct {
+	Name string
+	GUID string
+}
+
+type SequencePrices []SequencePrice
+type SequencePrice struct {
+	Sequence int32
+	Price float64
+}
+
+type SizeSequencePricingRules []SizeSequencePricingRule
+type SizeSequencePricingRule struct {
+	SizeName string
+	SizeGUID string
+	SequencePrices SequencePrices
+}
+
+type TimeSpecificPrices []TimeSpecificPrice
+type TimeSpecificPrice struct {
+	TimeSpecificPrice float64
+	BasePrice float64
+	Schedule Schedules
+}
+
+type PricingRules struct {
+	TimeSpecificPricingRules TimeSpecificPrices
+	SizeSpecificPricingGUID string
+	SizeSequencePricingRules SizeSequencePricingRules
+}
+
+type SalesCategory struct {
+	Name string
+	GUID string
+}
+
+type Alcohol struct {
+	ContainsAlcohol string
+}
+
+type ContentAdvisories struct {
+	Alcohol Alcohol
+}
+
+type Portions []Portion
+type Portion struct {
+	Name string
+	GUID string
+	ModifierGroupReferences ints
+}
+
+type PreModifiers []PreModifier
+type PreModifier struct {
+	Name string
+	GUID string
+	MultiLocationID string
+	FixedPrice float64
+	MultiplicationFactor float64
+	DisplayMode string
+	PosName string
+	PosButtonColorLight string
+	PosButtonColorDark string
+}
+
+type PreModifierGroup struct {
+	Name string
+	GUID string
+	MultiLocationID string
+	PreModifiers PreModifiers
+}
+
+type ModifierGroup struct {
+	Name string
+	GUID string
+	ReferenceID int
+	MultiLocationID string
+	MasterID int64 // DEPRECATED
+	PosName string
+	PosButtonColorLight string
+	PosButtonColorDark string
+	PricingStrategy string
+	PricingRules PricingRules
+	DefaultOptionsChargePrice string
+	DefaultOptionsSubstitutionPricing string
+	MinSelections int
+	MaxSelections int
+	RequiredMode string
+	IsMultiSelect bool
+	PreModifierGroupReference int
+	ModifierOptionReferences ints
+}
+
+type Metadata struct {
+	RestaurantGUID string
+	LastUpdated string
+}
 
 type MenuItems []MenuItem
 type MenuItem struct {
@@ -21,19 +132,19 @@ type MenuItem struct {
 	Image string
 	Price float64
 	PricingStrategy string
-	PricingRules menus.PricingRules
+	PricingRules PricingRules
 	IsDeferred bool
 	IsDiscountable bool
-	SalesCategory menus.SalesCategory
+	SalesCategory SalesCategory
 	TaxInfo strings
 	TaxInclusion string
-	ItemTags menus.ItemTags
+	ItemTags ItemTags
 	Plu string
 	Sku string
 	Calories int
-	ContentAdvisories menus.ContentAdvisories
+	ContentAdvisories ContentAdvisories
 	UnitOfMeasure string
-	Portions menus.Portions
+	Portions Portions
 	PrepTime int32
 	PrepStations strings
 	ModifierGroupReferences ints
@@ -59,7 +170,7 @@ type MenuGroup struct {
 	PosButtonColorLight string
 	PosButtonColorDark string
 	Image string
-	ItemTags menus.ItemTags
+	ItemTags ItemTags
 	MenuGroups MenuGroups
 	MenuItems MenuItems
 }
@@ -75,7 +186,7 @@ type Menu struct {
 	PosButtonColorDark string
 	HighResImage string
 	Image string
-	Availability menus.Availability
+	Availability Availability
 	MenuGroups MenuGroups
 }
 
